b893bb8fec8296886bb72529d764719c2af8c008
guerson345 <leonismogabriel@gmail.com>
2024-06-11 03:13:23 -0300

    Feat: formulário das transações iniciado

--- src/pages/table/table.tsx
+++ src/pages/table/table.tsx
@@ -1,7 +1,24 @@
+import { Container, Stack } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 export default function Table() {
     return (
-        <div>
-            <h1>Table</h1>
-        </div>
+        <Container className='mt-5' fluid='sm'>
+            <Stack gap={3}>
+                <Form>
+                    <Form.Group className='mb-3' controlId="formBasicEmail">
+                        <Form.Label>Nome</Form.Label>
+                        <Form.Control type="text" placeholder="Nome" name='name' />
+                        <Form.Label>Descrição</Form.Label>
+                        <Form.Control type="text" placeholder="Descrição" name='description' />
+                    </Form.Group>
+                    <Stack direction="horizontal" gap={3}>
+                        <Button type="reset" variant="secondary">Cancelar</Button>
+                        <Button type="submit" variant="primary">Salvar</Button>
+                    </Stack>
+                </Form>
+            </Stack>
+        </Container>
     );
 }
--- src/pages/transictions/transictions.tsx
+++ src/pages/transictions/transictions.tsx
@@ -1,7 +1,24 @@
+import { Container, Stack } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 export default function Transictions() {
     return (
-        <div>
-            <h1>Transactions</h1>
-        </div>
+        <Container className='mt-5' fluid='sm'>
+            <Stack gap={3}>
+                <Form>
+                    <Form.Group className='mb-3' controlId="formBasicEmail">
+                        <Form.Label>Transição</Form.Label>
+                        <Form.Control type="text" placeholder="Transição" name='transiction' />
+                        <Form.Label>Descrição</Form.Label>
+                        <Form.Control type="text" placeholder="Descrição" name='transictionDescription' />
+                    </Form.Group>
+                    <Stack direction="horizontal" gap={3}>
+                        <Button type="reset" variant="secondary">Cancelar</Button>
+                        <Button type="submit" variant="primary">Salvar</Button>
+                    </Stack>
+                </Form>
+            </Stack>
+        </Container>
     );
 }
