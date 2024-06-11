2684e9e0406f6de121e041704b3c5615ccc958f4
guerson345 <leonismogabriel@gmail.com>
2024-06-11 03:11:15 -0300

    Feat: agora a pagina de categorias tem um formulário

--- src/pages/categories/categories.tsx
+++ src/pages/categories/categories.tsx
@@ -1,7 +1,24 @@
+import { Container, Stack } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 export default function Categories() {
     return (
-        <div>
-            <h1>Categories</h1>
-        </div>
+        <Container className='mt-5' fluid='sm'>
+            <Stack gap={3}>
+                <Form>
+                    <Form.Group className='mb-3' controlId="formBasicEmail">
+                        <Form.Label>Categoria</Form.Label>
+                        <Form.Control type="text" placeholder="Categoria" name='category' />
+                        <Form.Label>Descrição</Form.Label>
+                        <Form.Control type="text" placeholder="Descrição" name='categoryDescription' />
+                    </Form.Group>
+                    <Stack direction="horizontal" gap={3}>
+                        <Button type="reset" variant="secondary">Cancelar</Button>
+                        <Button type="submit" variant="primary">Salvar</Button>
+                    </Stack>
+                </Form>
+            </Stack>
+        </Container>
     );
 }
