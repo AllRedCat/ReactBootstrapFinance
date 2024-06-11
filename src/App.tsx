0823fbdc449ff9ed251485d6ee8b38f464ba96e3
guerson345 <leonismogabriel@gmail.com>
2024-06-11 02:12:24 -0300

    Feat: navbar agora está funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './components/NavBar';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import Home from './pages/home/home';
+import Account from './pages/account/account';
+import Categories from './pages/categories/categories';
+import Transictions from './pages/transictions/transictions';
+import Tables from './pages/table/table';
 
 function App() {
+  let Component;
+  switch (window.location.pathname) {
+    case '/':
+      Component = <Home />;
+      break;
+    case '/accounts':
+      Component = <Account />;
+      break;
+    case '/categories':
+      Component = <Categories />;
+      break;
+    case '/transfections':
+      Component = <Transictions />;
+      break;
+    case '/tables':
+      Component = <Tables />;
+      break;
+    default:
+      break;
+  }
 
   return (
     <>
       <NavigationBar />
+      {Component}
       <h1>Hello world!</h1>
     </>
   )
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,17 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
-// import {
-//     createBrowserRouter,
-//     RouterProvider,
-//     Route,
-//     Link,
-// } from "react-router-dom";
 
 export default function NavigationBar() {
     return (
         <Navbar bg="dark" data-bs-theme="dark" expand="lg">
             <Container>
-                <Navbar.Brand href="/">React App</Navbar.Brand>
+                <Navbar.Brand href="/">Home</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/accounts">Accounts</Nav.Link>
+                        <Nav.Link href="/categories">Categories</Nav.Link>
+                        <Nav.Link href="/transfections">Transfections</Nav.Link>
+                        <Nav.Link href="/tables">Tables</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
