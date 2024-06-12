c8f9969b60db9334bcfb8961df45d703452329c5
guerson345 <leonismogabriel@gmail.com>
2024-06-12 16:32:54 -0300

    desisti de usar o cache local

--- src/pages/account/account.tsx
+++ src/pages/account/account.tsx
@@ -5,69 +5,23 @@ import Table from 'react-bootstrap/Table';
 import React, { useState, useEffect } from 'react';
 
 export default function Account() {
-    // // const [accoutData, setAccoutData] = useState<{ name: string; description: string }[]>([]);
-
-    // let [accoutData, setAccoutData] = useState([
-    //     { name: 'Banco do Brasil', description: 'Conta corrente' },
-    //     { name: 'Nubank', description: 'Conta digital' }
-    // ]);
-
-    // const saveDatainCache = (key: string, array: any) => {
-    //     const arrayCache = JSON.stringify(array);
-    //     localStorage.setItem(key, arrayCache);
-    //     console.log('Salvo no cache'); // teste
-    // };
-
-    // const getDataFromCache = (key: string) => {
-    //     const arrayCache = localStorage.getItem(key);
-    //     // return arrayCache ? JSON.parse(arrayCache) : [];
-    //     let AccountData = arrayCache;
-    //     if (AccountData) {
-    //         AccountData = JSON.parse(AccountData);
-    //     }
-    //     console.log(AccountData);
-    // };
-
-
-    // function submitHandler(event: React.FormEvent<HTMLFormElement>) {
-    //     event.preventDefault();
-
-    //     const name = event.currentTarget.name.value;
-    //     const description = event.currentTarget.description.value;
-
-    //     setAccoutData(prevData => [
-    //         ...prevData,
-    //         { name, description }
-    //     ]);
-
-    //     saveDatainCache('accoutData', accoutData);
-    //     getDataFromCache('accoutData');
-    //     console.log(accoutData);
-    // }
-
     const [accountData, setAccountData] = useState([
         { name: 'Banco do Brasil', description: 'Conta corrente' },
         { name: 'Nubank', description: 'Conta digital' }
     ]);
 
-    // Carregar dados do localStorage ao montar o componente
-    useEffect(() => {
-        const storedData = localStorage.getItem('accoutData');
-        if (storedData) {
-            setAccountData(JSON.parse(storedData));
-        }
-    }, []);
-
-    // Salvar dados no localStorage sempre que accountData for atualizada
-    useEffect(() => {
-        localStorage.setItem('accoutData', JSON.stringify(accountData));
-    }, [accountData]);
-
     function submitHandler(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
+
         const name = event.currentTarget.name.value;
         const description = event.currentTarget.description.value;
-        setAccountData(prevData => [...prevData, { name, description }]);
+
+        setAccountData(prevData => [
+            ...prevData,
+            { name, description }
+        ]);
+
+        console.log(accountData);
     }
 
     return (
@@ -105,3 +59,4 @@ export default function Account() {
         </Container>
     );
 }
+
