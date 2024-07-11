2927ab08ff93eaaef31456bd8ce6faf55fd672dc
guerson345 <leonismogabriel@gmail.com>
2024-07-11 19:54:19 -0300

    Explicação

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,6 +2,8 @@ import { Row, Col, Container } from 'react-bootstrap';
 import './home.css';
 import { Chart } from "react-google-charts";
 
+
+
 export default function Home() {
 
     const data = [
@@ -13,26 +15,28 @@ export default function Home() {
     ];
 
     const dataGeneral = [
+        // nome da coluna, valor1, valor2, valor3
         [
             "Day",
             "Guardians of the Galaxy",
             "The Avengers",
             "Transformers: Age of Extinction",
-          ],
-          [1, 37.8, 80.8, 41.8],
-          [2, 30.9, 69.5, 32.4],
-          [3, 25.4, 57, 25.7],
-          [4, 11.7, 18.8, 10.5],
-          [5, 11.9, 17.6, 10.4],
-          [6, 8.8, 13.6, 7.7],
-          [7, 7.6, 12.3, 9.6],
-          [8, 12.3, 29.2, 10.6],
-          [9, 16.9, 42.9, 14.8],
-          [10, 12.8, 30.9, 11.6],
-          [11, 5.3, 7.9, 4.7],
-          [12, 6.6, 8.4, 5.2],
-          [13, 4.8, 6.3, 3.6],
-          [14, 4.2, 6.2, 3.4],
+        ],
+        // linha horizontal, Guardians of the Galaxy, The Avengers, Transformers: Age of Extinction
+        [1, 37.8, 80.8, 41.8],
+        [2, 30.9, 69.5, 32.4],
+        [3, 25.4, 57, 25.7],
+        [4, 11.7, 18.8, 10.5],
+        [5, 11.9, 17.6, 10.4],
+        [6, 8.8, 13.6, 7.7],
+        [7, 7.6, 12.3, 9.6],
+        [8, 12.3, 29.2, 10.6],
+        [9, 16.9, 42.9, 14.8],
+        [10, 12.8, 30.9, 11.6],
+        [11, 5.3, 7.9, 4.7],
+        [12, 6.6, 8.4, 5.2],
+        [13, 4.8, 6.3, 3.6],
+        [14, 4.2, 6.2, 3.4],
     ];
 
     const optionsIn = {
@@ -41,7 +45,7 @@ export default function Home() {
         colors: ["#008FFB"],
         legend: { position: "none" },
     };
-    
+
     const optionsOut = {
         title: "Saídas",
         titleTextStyle: { fontName: "JetBrains Mono", fontSize: 14 },
@@ -76,7 +80,8 @@ export default function Home() {
                         data={data}
                         options={optionsIn}
                         width="100%"
-                        height="100%"
+                        min-height="100px"
+                        max-height="100%"
                     />
                 </Col>
                 <Col className="Box">
@@ -106,7 +111,7 @@ export default function Home() {
                     <Chart
                         chartType="Line"
                         width="100%"
-                        height="100%"
+                        height="90%"
                         data={dataGeneral}
                         options={optionsGeneral}
                     />
